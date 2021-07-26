9bd0c3c221df06ee3ba8a1dbf4996f602a10323c
Hero <herorfaat.mohammed@gmail.com>
2021-07-26 17:28:34 +0300

    Adds dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,32 @@
+/**
+ * Required External Modules
+ */
+
+import * as dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import Logger from './configs/logger';
 import swaggerUi from 'swagger-ui-express';
 import morganMiddleware from './configs/morgan.middleware';
 
+/**
+ * App Variables
+ */
+dotenv.config();
+
+if (!process.env.APP_PORT) {
+  process.exit(1);
+}
+
+const PORT: number = parseInt(process.env.APP_PORT as string, 10);
+
 const app = express();
 
+/**
+ *  App Configuration
+ */
+
 app.use(morganMiddleware);
+app.use(express.json());
 
 /**
  * Logger test
@@ -21,14 +42,17 @@ app.get('/logger', (_, res) => {
 });
 
 /**
- * --------------  SWAGGER  --------------
+ * SWAGGER
  */
 app.use('/docs', swaggerUi.serve, async (_req: Request, res: Response) => {
   return res.send(swaggerUi.generateHTML(await import('../swagger.json')));
 });
 
-const PORT = 3000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+/**
+ * Server Activation
+ */
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
