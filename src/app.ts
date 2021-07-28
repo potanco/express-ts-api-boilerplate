602977db9c5b0ce8e1499ccd12b014a86f322e9b
Hero <herorfaat.mohammed@gmail.com>
2021-07-28 15:13:56 +0300

    adds routes

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import cors from 'cors';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import helmet from 'helmet';
-import swaggerUi from 'swagger-ui-express';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 import morganMiddleware from './middleware/morgan.middleware';
+import Logger from './common/logger';
+import routes from './routes';
 
 import { errorHandler } from './middleware/error.middleware';
 import { notFoundHandler } from './middleware/not-found.middleware';
 import { rateLimiter } from './middleware/rateLimiter.middleware';
-import Logger from './common/logger';
 import { ConfigService } from './common/config';
 
 const configService = new ConfigService('.env');
@@ -19,9 +19,14 @@ class App {
 
   constructor() {
     this.express = express();
+
+    // Logger -Morgan Middleware
+    this.express.use(morganMiddleware);
+
+    this.express.use(routes);
+    
     this.middleware();
     this.configure();
-    this.routes();
     this.setupDatabaseConnection();
   }
 
@@ -33,53 +38,25 @@ class App {
 
   // Configure Express middleware.
   private middleware(): void {
-    /**
-     * Body parsing Middleware
-     */
+    // Body parsing Middleware
     this.express.use(express.json());
     this.express.use(express.urlencoded({ extended: false }));
 
-    /**
-     * Error Handling Middleware
-     */
+    // Error Handling Middleware
     this.express.use(errorHandler);
     this.express.use(notFoundHandler);
 
-    /**
-     * Rate Limiter Middleware
-     */
+    // Rate Limiter Middleware
     this.express.use(rateLimiter);
-
-    /**
-     * Morgan Middleware
-     */
-    this.express.use(morganMiddleware);
   }
 
-  private routes(): void {
-    this.express.get('/', (req, res, next) => {
-      res.send('Typescript App works!!');
-    });
-    /**
-     * Swagger
-     */
-    this.express.get(
-      '/docs',
-      swaggerUi.serve,
-      async (_req: Request, res: Response) => {
-        return res.send(
-          swaggerUi.generateHTML(await import('../swagger.json'))
-        );
-      }
-    );
-  }
-
-   private setupDatabaseConnection() {
+  //Database Connection
+  private setupDatabaseConnection() {
     const connection = mongoose.connection;
-    connection.on("connected", () => {
+    connection.on('connected', () => {
       Logger.info('⚡️[Mongo] Connection Established');
     });
-    connection.on("reconnected", () => {
+    connection.on('reconnected', () => {
       Logger.info('⚡️[Mongo] Connection Reestablished');
     });
 
@@ -94,4 +71,5 @@ class App {
     run().catch((error) => Logger.error(`${error}`));
   }
 }
+
 export default new App().express;
